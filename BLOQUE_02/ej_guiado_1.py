860b318e0bdc9139af9166787c991b4135dc1f56
rafael <rafardrgz24@gmail.com>
2025-10-17 13:44:35 +0200

    b2_new

---
+++ BLOQUE_02/ej_guiado_1.py
@@ -0,0 +1,7 @@
+"""
+Define una función llamada mostrar_saludo().
+Dentro de la función, imprime: "Hola, este es tu primer programa con funciones.".
+Añade un docstring explicando qué hace la función.
+Llama a la función varias veces desde el programa principal.
+"""
+
