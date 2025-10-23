950fbee5ed1e8e55d342549b9a001be6a3902d0a
Rafael <rafardrgz24@gmail.com>
2025-10-23 19:18:11 +0200

    All activities with docstrings - commit

--- BLOQUE_01/b1_5_1_calculadora_condicional.py
+++ BLOQUE_01/b1_5_1_calculadora_condicional.py
@@ -1,12 +1,13 @@
-# Crea un script que:
-# 1. Pida al usuario dos números (num1, num2).
-# 2. Pida elegir una operación: suma, resta, multiplicación o división.
-# 3. Use if...elif...else para realizar la operación seleccionada.
-# 4. Si elige una opción inválida, mostrar "Operación no reconocida".
-# 👉 Ejemplo de salida:
-#   Elige operación (suma, resta, multiplicacion, division): suma
-#   Resultado: 25
-
+"""
+Crea un script que:
+1. Pida al usuario dos números (num1, num2).
+2. Pida elegir una operación: suma, resta, multiplicación o división.
+3. Use if...elif...else para realizar la operación seleccionada.
+4. Si elige una opción inválida, mostrar "Operación no reconocida".
+ · Ejemplo de salida:
+Elige operación (suma, resta, multiplicacion, division): suma
+Resultado: 25
+"""
 num1 = float(input("Escribe el número 1 -> "))
 num2 = float(input("Escribe el número 2 -> "))
 # Utilizamos .lower() para que el usuario pueda introducir mayúscula o minúscula y la condición lo interprete de
