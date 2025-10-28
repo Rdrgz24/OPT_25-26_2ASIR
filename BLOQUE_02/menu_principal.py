e146108384426e281e50bf73e1e815905ff5aec7
Rafael <rafardrgz24@gmail.com>
2025-10-28 13:35:31 +0100

    b2 activites - not complete

---
+++ BLOQUE_02/menu_principal.py
@@ -0,0 +1,9 @@
+def mostrar_menu():
+    """Muestra un menú principal - sin entradas por teclado, solo visualizar."""
+    print("MENÚ PRINCIPAL - OPCIONES")
+    print("1. Jugar")
+    print("2. Configuración")
+    print("3. Salir\n")
+
+mostrar_menu()
+mostrar_menu()
