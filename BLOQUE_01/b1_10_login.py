e3cfe79a184d3975fd5950485f81112ca58c208c
rafael <rafardrgz24@gmail.com>
2025-10-31 14:16:35 +0100

    b1 - 10 complete - no dictionary

--- BLOQUE_01/b1_10_login.py
+++ BLOQUE_01/b1_10_login.py
@@ -1,6 +1,6 @@
 """
 Este programa permite registrar un usuario con un email y contraseña,
-y luego iniciar sesión con él.
+y luego iniciar sesión con él. prueba
 """
 
 # Bucle principal del menú (1, 2, 3)
