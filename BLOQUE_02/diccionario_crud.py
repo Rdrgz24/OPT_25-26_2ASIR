ddac1e58ed8cc4e2f39a0b0550591a99e2bb8111
rafael <rafardrgz24@gmail.com>
2025-11-17 14:23:44 +0100

    b2

--- BLOQUE_02/diccionario_crud.py
+++ BLOQUE_02/diccionario_crud.py
@@ -5,25 +5,34 @@ people = {}  # Main dictionary: {nif: {name, age, city, profession}}
 def create_person(persona):
     # pass  # TODO: Ask for ID, name, age, city, profession and add to people
     if len(dni) == 9:
-        key = persona.get("DNI")
+        key = persona.get("dni")
         people[key] = persona
     else:
         print("DNI incorrecto.")
 
-def read_people(buscar_dni):
+def read_people(dnis):
     # pass  # TODO: Loop through people and print their info
-    if buscar_dni in people:
-        print(people[buscar_dni])
-    else:
-        print("no existe dni")
+    for dni in dnis:
+        if dni in people:
+            print(people[dni])
+        else:
+            print("No se encuentra")
 
 def update_person(tudni):
     """Update information of an existing person."""
     # pass  # TODO: Ask for ID, check if exists, and update fields
-    key = persona.get("DNI")
-    key2 = persona.get("midni")
-    print(f"Has seleccionado el usuario, {key}")
-
+    if tudni in people:
+        key = persona.get("dni")
+        print(f"Datos actuales -> {people[tudni]}")
+        opcion = input("¿Qué dato quieres editar? Nombre, edad, ciudad o profesion.")
+        if opcion.lower() in ["nombre", "edad", "ciudad", "profesion"]:
+            valor = input(f"Introduce el nuevo valor para {opcion}")
+            people[tudni][opcion] = valor
+            print("Dato actualizado correctamente.")
+        else:
+            print("Opción no válida.")
+    else:
+        print(f"DNI no encontrado.")
 
 def delete_person():
     # pass  # TODO: Ask for ID and remove from the dictionary if exists
@@ -53,18 +62,17 @@ while option != "5":
             edad = input("Introduce tu edad -> ")
             ciudad = input("Introduce tu ciudad -> ")
             prof = input("Introduce tu profesion -> ")
-            persona = {"DNI": dni, "Nombre": nombre, "Edad": edad, "Ciudad": ciudad, "Profesion": prof}
+            persona = {"dni": dni, "nombre": nombre, "edad": edad, "ciudad": ciudad, "profesion": prof}
             create_person(persona)
         case "2":
+            buscar = 0
             dnis = []
-            dni = 1
-            while dni != -1:
+            while buscar != -1:
                 dni = input("Introduce un DNI o -1 para parar -> ")
                 dnis.append(dni)
             print("Bucle terminado")
             read_people(dnis)
         case "3":
-            print(people)
             tudni = input("¿Qué usuario quieres modificar?")
             update_person(tudni)
         case "4":
